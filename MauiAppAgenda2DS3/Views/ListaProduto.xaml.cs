4bbfdb3f03e2ed1ed2cefff56c5d89f8b38dcda5
Jonathan Kadekaro <jonathan_supernatural@hotmail.com>
2025-08-24 15:25:15 -0300

    att

--- MauiAppAgenda2DS3/Views/ListaProduto.xaml.cs
+++ MauiAppAgenda2DS3/Views/ListaProduto.xaml.cs
@@ -1,13 +1,28 @@
+using MauiAppAgenda2DS3.Models;
+using System.Collections.ObjectModel;
+
 namespace MauiAppAgenda2DS3.Views;
 
 public partial class ListaProduto : ContentPage
 {
+	ObservableCollection<Produto> lista = new ObservableCollection<Produto>();
+
 	public ListaProduto()
 	{
 		InitializeComponent();
-	}
+		
+		lst_produtos.ItemsSource = lista;
+    }
 
-	private void ToolbarItem_Clicked(object sender, EventArgs e)
+	protected async override void OnAppearing()
+	{
+		List<Produto> tmp = await App.Db.GetAll();
+
+		tmp.ForEach( i => lista.Add (i) );
+
+    }
+
+    private void ToolbarItem_Clicked(object sender, EventArgs e)
 	{
 
 		try
@@ -23,5 +38,29 @@ public partial class ListaProduto : ContentPage
 
 		}
 	}
-	
+
+    private async void txt_search_TextChanged(object sender, TextChangedEventArgs e)
+    {
+		string q = e.NewTextValue;
+
+		lista.Clear();
+
+        List<Produto> tmp = await App.Db.Search(q);
+
+        tmp.ForEach(i => lista.Add(i));
+    }
+
+    private void ToolbarItem_Clicked_1(object sender, EventArgs e)
+    {
+		double soma = lista.Sum(i => i.Total);
+
+		string msg = $"O total é {soma:C}";
+
+		DisplayAlert("Total dos Produtos", msg, "OK");
+    }
+
+    private void MenuItem_Clicked(object sender, EventArgs e)
+    {
+
+    }
 }
