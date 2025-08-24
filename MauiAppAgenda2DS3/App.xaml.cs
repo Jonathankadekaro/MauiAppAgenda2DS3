fd107658a32da731c60858a13cf33106c5d8f9a6
Jonathan Kadekaro <jonathan_supernatural@hotmail.com>
2025-08-24 14:34:59 -0300

    Lista e novo produto

--- MauiAppAgenda2DS3/App.xaml.cs
+++ MauiAppAgenda2DS3/App.xaml.cs
@@ -1,7 +1,30 @@
-﻿namespace MauiAppAgenda2DS3
+﻿using MauiAppAgenda2DS3.Helpers;
+
+namespace MauiAppAgenda2DS3
 {
     public partial class App : Application
     {
+        static SQLiteDatabaseHelper _db;
+
+        public static SQLiteDatabaseHelper Db 
+        {
+            get 
+            {
+                if (_db == null) 
+                {
+                    string path = Path.Combine(
+                        Environment.GetFolderPath(
+                         Environment.SpecialFolder.LocalApplicationData),   
+                           "banco_sqlite_compras.db3" );
+
+                    _db = new SQLiteDatabaseHelper(path);
+                
+                }                
+            
+                return _db;
+            }
+        
+        }
         public App()
         {
             InitializeComponent();
--- MauiAppAgenda2DS3/Views/NovoProduto.xaml.cs
+++ MauiAppAgenda2DS3/Views/NovoProduto.xaml.cs
@@ -1,3 +1,5 @@
+using MauiAppAgenda2DS3.Models;
+
 namespace MauiAppAgenda2DS3.Views;
 
 public partial class NovoProduto : ContentPage
@@ -6,4 +8,31 @@ public partial class NovoProduto : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+
+		try
+		{
+			Produto p = new Produto
+			{
+				Descricao = txt_descricao.Text,
+				Quantidade = Convert.ToDouble(txt_quantidade.Text),
+				Preco = Convert.ToDouble(txt_preco.Text)
+
+			};	
+
+			await App.Db.Insert(p);
+
+			await DisplayAlert("Sucesso", "Registro concluido", "OK");
+
+
+        } catch (Exception ex)
+		{
+
+			await DisplayAlert("Ops", ex.Message, "OK");
+        }
+
+
+    }
 }
